d9b98ea8b8556bc39fb661763a1cbedae22c0d95
colin <trowa020@gmail.com>
2021-10-19 19:28:24 +0900

    dp 2문제

---
+++ src/baekjoon/dp/_10870/Main.java
@@ -0,0 +1,23 @@
+package baekjoon.dp._10870;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] dp = new int[21];
+        dp[0] = 0;
+        dp[1] = 1;
+        for (int i = 2; i < dp.length; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        System.out.println(dp[n]);
+    }
+}
+
+// n = 1 1
+// n = 2 2
+// n = 3
